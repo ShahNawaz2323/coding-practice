d6df6d819ea0934812e481a65255162b8926ef50
Burn-the-asses <shahnawazwarriach2323@gmail.com>
2022-05-29 12:31:51 +0500

    basic csharp code practices

---
+++ Csharp_Code_Practice/1.basic/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello:");
+            System.Console.WriteLine("Shahnawaz");
+
+            System.Console.WriteLine("Write a C# Sharp program to print the sum of two numbers");
+            int a = 20;
+            int b = 10;
+
+            var sum = a + b;
+            System.Console.WriteLine(sum);
+
+            System.Console.WriteLine("Write a C# Sharp program to print the division of two numbers");
+            // int a = 20;
+            // int b = 10;
+
+            var div = a / b;
+            System.Console.WriteLine(div);
+
+        }
+    }
+}
+
---
+++ Csharp_Code_Practice/2.variables/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNum;
+            myNum = 15;
+            string name = "shah";
+            Console.WriteLine(name);
+            Console.WriteLine(myNum);
+            myNum = 20;
+            System.Console.WriteLine(myNum);
+            System.Console.WriteLine("Hello " + name);
+            string name2 = "nawaz";
+            System.Console.WriteLine(name + name2);
+
+            var fullname = name + name2;
+            System.Console.WriteLine(fullname);
+
+            int x = 5, y= 7, z=50;
+            System.Console.WriteLine(x + y + z);
+
+        }
+    }
+}
