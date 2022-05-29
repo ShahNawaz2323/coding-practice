30b97ece338b1ad6c47088246db623ef9ce89e05
Burn-the-asses <shahnawazwarriach2323@gmail.com>
2022-05-29 11:56:55 +0500

    stack practice

---
+++ CSharp_Dotnet_Practices/Mosh_Hamdani_Practice/24.StackPrac/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+
+namespace StackPrac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          // Create and initialize a stack
+          Stack stk = new Stack();
+          stk.Push("Welcome");
+          stk.Push("Tutlane");
+          stk.Push(20.5f);
+          stk.Push(10);
+          stk.Push(null);
+          stk.Push(100);
+          Console.WriteLine("Number of Elements in Stack: {0}", stk.Count);
+          Console.WriteLine("******Stack Elements******");
+          
+        //   // Access Stack Elements
+        //   foreach (var item in stk)
+        //   {
+        //      Console.WriteLine(item);
+        //   }
+
+          //pop Stack Element
+          while(stk.Count > 0 )
+          {
+              System.Console.WriteLine(stk.Pop());
+          }
+          Console.WriteLine("Number of Elements in Stack: {0}", stk.Count);
+          Console.ReadLine();
+        }
+    }
+}
