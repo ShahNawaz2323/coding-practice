02106cd11fd20537498e0802f846681b5efdebe0
Burn-the-asses <shahnawazwarriach2323@gmail.com>
2022-05-30 11:06:47 +0500

    Csharp data types and basic exercises.

---
+++ Csharp_Code_Practice/4.DataTypes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****************Variable Data Types**************");
+            int myNum = 5 ;
+            double myDouble = 10.2f;
+            char mycharacter = 'D';
+            bool myBool = true;
+            string myText = "Hello";
+            long myNum1 = 150000000L;
+            float myNum2 = 5.75F;
+
+            System.Console.WriteLine(myNum);
+            System.Console.WriteLine(myDouble);
+            System.Console.WriteLine(mycharacter);
+            System.Console.WriteLine(myBool);
+            System.Console.WriteLine(myText);
+            System.Console.WriteLine(myNum1);
+            System.Console.WriteLine(myNum2);
+
+        }
+    }
+}
