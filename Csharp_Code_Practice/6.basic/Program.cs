da5c73b8bab4e88fba65562507dd9e0740f933c5
Burn-the-asses <shahnawazwarriach2323@gmail.com>
2022-05-31 10:25:53 +0500

    type casting and basic csharp coding practices.

---
+++ Csharp_Code_Practice/6.basic/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a C# Sharp program to print on screen the output of adding, subtracting, multiplying and dividing,entered by the user");
+            System.Console.Write("Enter First Number: ");
+            int x = Convert.ToInt32(System.Console.ReadLine());
+            System.Console.Write("Enter Second Number: ");
+            int y = Convert.ToInt32(System.Console.ReadLine());
+
+            System.Console.WriteLine("Operation on the numbers");
+            System.Console.WriteLine("{0} + {1} = {2}",x,y, x+y);
+            System.Console.WriteLine("{0} + {1} = {2}", x,y, x-y);
+            System.Console.WriteLine("{0} + {1} = {2}",x,y, x*y);
+            System.Console.WriteLine("{0} + {1} = {2}",x,y, x%y);
+            
+        }
+    }
+}
