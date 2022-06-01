8b23369153ebdaa7971deef08c3df938312a9a97
Burn-the-asses <shahnawazwarriach2323@gmail.com>
2022-06-01 09:58:27 +0500

    Csharp Practice of user inputs and basic coding.

---
+++ Csharp_Code_Practice/7.basic/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***********Find The Multiples of given numbers");
+
+            System.Console.Write("Give Number which you want to find multiple: ");
+            int v = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= 10; i++)
+            {
+                int z;
+                z = v;
+                z = v*i; 
+                System.Console.WriteLine("{0} * {1} = {2}",v,i,z);
+            }
+
+            System.Console.WriteLine("Write a C# Sharp program that takes four numbers as input to calculate and print the average.");
+                int x =10 , y = 15 ,j = 20 , a = 30;
+                int u = 4;
+                int result = (x+y+j+a)/u;
+                System.Console.WriteLine("the average of {0},{1},{2},{3} is : {4}",x,y,j,a,result);
+        }
+    }
+}
---
+++ Csharp_Code_Practice/8.UserInput/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**************User Inputs");
+            System.Console.WriteLine("Enter Your Name:");
+            string name = Console.ReadLine();
+
+            System.Console.WriteLine("User Name is : " + name);
+
+            System.Console.WriteLine("Enter Your Age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            System.Console.WriteLine("User Age : " + age);
+
+            System.Console.WriteLine("User Name: {0} ,and age is: {1}", name,age);
+
+        }
+    }
+}
