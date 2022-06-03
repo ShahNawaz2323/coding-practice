deef05d4ab5067c8d7f6a642d623f0c2f1881691
Burn-the-asses <shahnawazwarriach2323@gmail.com>
2022-06-03 09:09:15 +0500

    basic code practice and Math functions.

---
+++ Csharp_Code_Practice/11.basic/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a C# program to convert from celsius degrees to Kelvin and Fahrenheit.");
+            // kelvin = celsius + 273
+            // fahrenheit = celsius x 18 / 10 + 32
+            int k = 273;
+            int f = 1;
+
+            System.Console.Write("Tell temperature in Celsius to calculate : ");
+            int v1 = Convert.ToInt32(Console.ReadLine());
+
+            int result1 = v1+k;
+            System.Console.WriteLine("Kelvin is : {0}",result1);
+
+            f = v1*18/10+32;
+            System.Console.WriteLine("Fahrenheit is: {0}",f);
+        }
+    }
+}
---
+++ Csharp_Code_Practice/12.Mathfun/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Mathfun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*********************Math functions****************************");
+            System.Console.WriteLine(Math.Max(10,5));
+            System.Console.WriteLine(Math.Min(10,5));
+            System.Console.WriteLine(Math.Sqrt(64));
+            System.Console.WriteLine(Math.Abs(-5.6));
+            System.Console.WriteLine(Math.Round(9.99));
+        }
+    }
+}
