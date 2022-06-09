1ce43b0dfcf6bda68f80f50ee3895dc980d956fe
Burn-the-asses <shahnawazwarriach2323@gmail.com>
2022-06-09 10:25:53 +0500

    Strings and basic coding practice.

---
+++ Csharp_Code_Practice/23.ArraysPrac/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*************************Csharp Arrays Practice******************");
+            string[] cars = {"volvo" , "BMW", "Ford", "Mazda"};
+            System.Console.WriteLine(cars[0]);
+            System.Console.WriteLine("change the value by accessing  index");
+            cars[0] = "opel";
+            System.Console.WriteLine(cars[0]);
+            System.Console.WriteLine("************Length of  the array");
+            System.Console.WriteLine(cars.Length);
+
+            System.Console.WriteLine("****************Loop through an array************");
+            for (int i = 0; i < cars.Length; i++)
+            {
+                System.Console.WriteLine(cars[i]);
+            }
+            System.Console.WriteLine("****************Loop through an array with foreach************");
+            foreach (string item in cars)
+            {
+                System.Console.WriteLine(item);
+            }
+            //Sort a string
+            System.Console.WriteLine("****************Sorting of A String Array************");
+            Array.Sort(cars);
+            foreach (var item in cars)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            //sort an int
+            System.Console.WriteLine("****************Sorting of An Integer Array************");
+            int[] myNumbers = {1,2,5,2,6,2,6,4,3,7,7,9,5,10};
+            Array.Sort(myNumbers);
+            foreach (var item in myNumbers)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            System.Console.WriteLine("**********Find the Largest, smallest value, myNumbers****");
+            System.Console.WriteLine(myNumbers.Max());
+            System.Console.WriteLine(myNumbers.Min());
+            System.Console.WriteLine(myNumbers.Sum());
+        }
+    }
+}
---
+++ Csharp_Code_Practice/24.basic/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+namespace basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("*******************Find the size of file ***************");
+            FileInfo f = new FileInfo(@"C:\Users\shahn\Downloads\Documents\HEC.pdf");
+            string fl = f.Length.ToString();
+            int size1 = Convert.ToInt32(fl);
+            size1 /= 1024;
+            System.Console.WriteLine("\nSize of file is: "+ size1 + " KB");
+            
+            // string line = "Display the pattern like pyramid using the alphabet.";
+            // Console.WriteLine("\nOriginal String: " + line);
+            // string result = "";
+            // List<string> wordsList = new List<string>();
+            // string[] words = line.Split(new[] { " " }, StringSplitOptions.None);
+
+            // for (int i = words.Length - 1; i >= 0; i--)
+            // {
+            //     result += words[i] + " ";
+            // }
+
+            // wordsList.Add(result);
+            // foreach (String s in wordsList)
+            // {
+            //     Console.WriteLine("\nReverse String: " + s);
+            // }
+
+            // Console.Write("Input  a number(integer): ");
+            // int n = Convert.ToInt32(Console.ReadLine());
+            // int sum = 0;
+
+            // while (n != 0)
+            // {
+            //     sum += n % 10;
+            //     n /= 10;
+            // }
+            // System.Console.WriteLine("Sum of the digits of the said integer: " + sum);
+
+
+            //testing
+            // sum += n % 10;
+
+            // System.Console.WriteLine(sum);
+            // n /= 10;
+            // System.Console.WriteLine(n);
+            // sum = sum + n;
+            // System.Console.WriteLine(sum);
+
+            //     Console.WriteLine("\nSum of the first 500 prime numbers: ");
+            //        long sum = 0;
+            //         int ctr = 0;
+            //         int n = 2;
+            //         while (ctr < 500)
+            //           {
+            //             if (isPrime(n))
+            //             {
+            //                 sum += n;
+            //                 ctr++;
+            //             }
+            //             n++;
+            //         }
+
+            //         Console.WriteLine(sum.ToString());            
+
+        }
+
+        //  public static bool isPrime(int n)
+        //     {
+        //         int x = (int)Math.Floor(Math.Sqrt(n));
+
+        //         if (n == 1) return false;
+        //         if (n == 2) return true;
+
+        //         for (int i = 2; i <= x; ++i)
+        //         {
+        //             if (n % i == 0) return false;
+        //         }
+
+        //         return true;
+        //     }
+    }
+}
