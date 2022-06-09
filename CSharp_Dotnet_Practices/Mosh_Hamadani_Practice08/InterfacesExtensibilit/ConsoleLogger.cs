198278e4f513502c81b33bc27b9fafaab84b66b7
Burn-the-asses <shahnawazwarriach2323@gmail.com>
2022-06-09 12:59:47 +0500

    Extensibility using interfaces.

---
+++ CSharp_Dotnet_Practices/Mosh_Hamadani_Practice08/InterfacesExtensibilit/ConsoleLogger.cs
@@ -0,0 +1,18 @@
+using System;
+namespace InterfacesExtensibilit
+{
+    public class ConsoleLogger : ILogger
+    {
+        public void LogError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+        }
+
+        public void LogInfo(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ CSharp_Dotnet_Practices/Mosh_Hamadani_Practice08/InterfacesExtensibilit/ILogger.cs
@@ -0,0 +1,9 @@
+using System;
+namespace InterfacesExtensibilit
+{
+    public interface ILogger
+    {
+        void LogError(string message);
+        void LogInfo(string message);
+    }
+}
