239d3b6567ab4d644fe250172e52b5794b1ad33b
Burn-the-asses <shahnawazwarriach2323@gmail.com>
2022-06-15 09:47:04 +0500

    Csharp objects ,classes and basic coding practice.

---
+++ Csharp_Code_Practice/37.Basic/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Write a C# program to count a specified number in a given array of integers");
+            System.Console.Write("Input an integer : ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int[] myVar = {1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 9};
+            System.Console.WriteLine("Number of " + x + " Present in the array" );
+            System.Console.WriteLine(myVar.Count(n => n == x));
+            System.Console.WriteLine("Write a C# program to check if a number appears as either the first or last element of an array");
+            System.Console.WriteLine((x == myVar[0]) || (myVar[myVar.Length - 1] == x));
+
+            System.Console.WriteLine("Compute the sum of the array......");
+            int commute = 0  ;
+            // foreach (var item in myVar)
+            // {
+            //     commute += item;
+            // }
+            for (int i = 0 ; i < myVar.Length ; i++) { commute += myVar[i]; };
+            System.Console.WriteLine(commute);
+
+        }
+    }
+}
---
+++ Csharp_Code_Practice/38.MultiInterfaces/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MultiInterfaces
+{
+    public enum level
+    {
+        Low,
+        Medium,
+        High
+    }
+    interface ImyFirstInterface
+    {
+        void myMethod();
+    }
+    interface ImySecondInterface
+    {
+        void myOtherMethod();
+    }
+    //implement multiple Interface
+    class DemoClass : ImyFirstInterface, ImySecondInterface
+    {
+        public void myMethod()
+        {
+            System.Console.WriteLine("Interface First Method implementention ");
+        }
+
+        public void myOtherMethod()
+        {
+            System.Console.WriteLine("Second Interface implemented");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Multi-level interfacee");
+            // DemoClass myObj = new DemoClass();
+            // myObj.myMethod();
+            // myObj.myOtherMethod();
+            System.Console.WriteLine("Enum practice: ");
+            level myLevel = level.Low;
+            System.Console.WriteLine("You point is: {0}",myLevel);
+        }
+    }
+}
