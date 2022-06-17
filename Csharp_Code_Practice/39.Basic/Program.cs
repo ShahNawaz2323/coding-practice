b1e62bc06db700b3383a5a58ac7df539631d0e22
Burn-the-asses <shahnawazwarriach2323@gmail.com>
2022-06-17 10:38:09 +0500

    Working with files and bsic csharp code practice.

---
+++ Csharp_Code_Practice/39.Basic/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("******Write a C# program to check if the first element or the last element \n*****of the two arrays ( length 1 or more) are equal.");
+            int[] nums1 = {1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 1};
+           // Console.WriteLine("\nArray1: [{0}]", string.Join(", ", nums1));
+            int[] nums2 = {1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 5};
+            //Console.WriteLine("\nArray2: [{0}]", string.Join(", ", nums2));
+            
+            Console.WriteLine("\nCheck if the first element or the last element of the two arrays ( length 1 or more) are equal.");
+            System.Console.WriteLine((nums1[0].Equals(nums2[0])) || (nums1[nums1.Length -1 ].Equals(nums2[nums2.Length -1])));
+        }
+    }
+}
