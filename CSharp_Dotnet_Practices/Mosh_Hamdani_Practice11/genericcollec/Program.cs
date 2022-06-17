98881ecafbd1e84933fad813fad6fa3238bc9cf3
Burn-the-asses <shahnawazwarriach2323@gmail.com>
2022-06-17 11:12:34 +0500

    Generic type and generic collections

---
+++ CSharp_Dotnet_Practices/Mosh_Hamdani_Practice11/genericcollec/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace genericcollec
+{
+    public class Utilities
+    {
+        public int Max(int a , int b )
+        {
+            return a > b ? a : b;
+        }
+
+        public T Max<T>(T a , T b) where T : IComparable
+        {  
+            return a.CompareTo(b) > 0 ? a : b; 
+        }
+    }
+    public class GenericList<T>
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
