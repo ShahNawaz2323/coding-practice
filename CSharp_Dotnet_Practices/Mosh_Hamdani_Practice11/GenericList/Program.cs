c06dba355e601722c2a85bcc4163bc84eb4a12ed
Burn-the-asses <shahnawazwarriach2323@gmail.com>
2022-06-18 11:06:49 +0500

    generic list practices

---
+++ CSharp_Dotnet_Practices/Mosh_Hamdani_Practice11/GenericList/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Basic
+{
+    public class Nullable<T> where T : struct
+    {
+        private object _value;
+
+        public Nullable(T value)
+        {
+            _value = Value;
+        }
+
+        public bool HasValue
+        {
+            get{ return _value != null;}
+        }
+
+        public T GetValueOrDefault()
+        {
+            if (HasValue)
+                return (T) _value;
+            return default(T);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = new Nullable<int>(5);
+            System.Console.WriteLine("Has Value" + number.HasValue);
+            System.Console.WriteLine("Value : " + number);
+        }
+    }
+}
