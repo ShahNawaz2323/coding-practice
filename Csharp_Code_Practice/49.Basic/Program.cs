704eb02f752f7ff53cefd3f5abcb5c0845ac2640
Burn-the-asses <shahnawazwarriach2323@gmail.com>
2022-06-20 10:06:04 +0500

    Basic Csharp coding Practices.

---
+++ Csharp_Code_Practice/49.Basic/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+
+namespace Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x  = sort_numbers(new int[]  {-5,236,120,70,-5,-5,698,280});
+            foreach(var item in x)
+            {
+                System.Console.WriteLine(item.ToString());
+            }
+
+        }
+        public static int[] sort_numbers(int[]  arra)
+        {
+            int[] num = arra.Where(x=> x != -5).OrderBy(x => x).ToArray();
+            int ctr = 0 ;
+
+            return arra.Select(x=> x >= 0 ? num[ctr++] : -5).ToArray();
+        }
+    }
+}
