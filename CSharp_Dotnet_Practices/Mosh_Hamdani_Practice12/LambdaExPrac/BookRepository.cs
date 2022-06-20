7b745be9b551fc7cb9e38c89bd99546b222bf759
Burn-the-asses <shahnawazwarriach2323@gmail.com>
2022-06-20 19:20:08 +0500

    Web Development forms and inputs.

---
+++ CSharp_Dotnet_Practices/Mosh_Hamdani_Practice12/LambdaExPrac/BookRepository.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+namespace LambdaExPrac
+{
+    public class BookRepository
+    {
+        public List<Book> GetBooks()
+        {
+            return new List<Book>
+                {
+                    new Book() {Title =  "Title 1", Price = 5},
+                    new Book() {Title =  "Title 2", Price = 7},
+                    new Book() {Title =  "Title 3", Price = 17}
+                };
+        }
+    }
+}
---
+++ CSharp_Dotnet_Practices/Mosh_Hamdani_Practice12/LambdaExPrac/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LambdaExPrac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //args => expression
+            // number => number*number
+            // Func<int,int> square = number => number*number; 
+
+            const int factor = 5;
+           Func<int,int> multipler = n => n*factor;
+           var result = multipler(10);
+           System.Console.WriteLine(result);
+            //() => ....
+            //x =>...
+            // (x,y,z) => ...
+         }
+
+        // static int  square(int number)
+        // {
+        //     return number*number;
+        // }
+    }
+}
