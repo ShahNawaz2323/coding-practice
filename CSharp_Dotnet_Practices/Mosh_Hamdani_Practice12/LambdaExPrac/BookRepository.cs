b131a87b6e668ede3e75de86f42c4a9036d263ab
Burn-the-asses <shahnawazwarriach2323@gmail.com>
2022-06-21 12:26:52 +0500

    LambdaExpression

--- CSharp_Dotnet_Practices/Mosh_Hamdani_Practice12/LambdaExPrac/BookRepository.cs
+++ CSharp_Dotnet_Practices/Mosh_Hamdani_Practice12/LambdaExPrac/BookRepository.cs
@@ -3,14 +3,14 @@ namespace LambdaExPrac
 {
     public class BookRepository
     {
-        public List<Book> GetBooks()
-        {
-            return new List<Book>
-                {
-                    new Book() {Title =  "Title 1", Price = 5},
-                    new Book() {Title =  "Title 2", Price = 7},
-                    new Book() {Title =  "Title 3", Price = 17}
-                };
-        }
+        // public List<Book> GetBooks()
+        // {
+        //     return new List<Book>
+        //         {
+        //             new Book() {Title =  "Title 1", Price = 5},
+        //             new Book() {Title =  "Title 2", Price = 7},
+        //             new Book() {Title =  "Title 3", Price = 17}
+        //         };
+        //}
     }
 }
--- CSharp_Dotnet_Practices/Mosh_Hamdani_Practice12/LambdaExPrac/Program.cs
+++ CSharp_Dotnet_Practices/Mosh_Hamdani_Practice12/LambdaExPrac/Program.cs
@@ -11,10 +11,17 @@ namespace LambdaExPrac
             // number => number*number
             // Func<int,int> square = number => number*number; 
 
+            // Func<int,int> square = number=> number*number;
+            // System.Console.WriteLine(square(5));
             const int factor = 5;
-           Func<int,int> multipler = n => n*factor;
-           var result = multipler(10);
-           System.Console.WriteLine(result);
+            Func<int, int> multiplyer = n => n*factor;
+            var result = multiplyer(10);
+            System.Console.WriteLine(result);
+
+        //     const int factor = 5;
+        //    Func<int,int> multipler = n => n*factor;
+        //    var result = multipler(10);
+        //    System.Console.WriteLine(result);
             //() => ....
             //x =>...
             // (x,y,z) => ...
