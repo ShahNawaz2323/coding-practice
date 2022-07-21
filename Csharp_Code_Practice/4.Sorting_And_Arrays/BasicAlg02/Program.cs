16f381e6ce25cdba320b39498d5a01f72f1957bb
Burn-the-asses <shahnawazwarriach2323@gmail.com>
2022-07-21 10:16:58 +0500

    Csharp coding practice array and Algo

---
+++ Csharp_Code_Practice/4.Sorting_And_Arrays/BasicAlg02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BasicAlg02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***Write a C# Sharp program to remove the character in a given position of a given string****");
+            System.Console.WriteLine(test("Python",1));
+            Console.WriteLine(test("Python", 0));
+            Console.WriteLine(test("Python", 4));
+            Console.ReadLine();
+        }
+
+        //  public static string test(string str, int n)
+        // {
+        //     return str.Remove(n, 1);
+        // }
+        public static string test(string str, int n)
+        {
+            if (str.Length >= 2)
+            {
+                return str.Remove(n,1);
+            }
+            else
+            {
+                return "string is to small";
+            }
+
+        }
+    }
+}
