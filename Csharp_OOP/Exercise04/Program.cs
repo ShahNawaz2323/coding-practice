0806bbe6cb941a73592d8ba90e7705de372821b0
Burn-the-asses <shahnawazwarriach2323@gmail.com>
2022-08-14 10:57:23 +0500

    Csharp oop practice

---
+++ Csharp_OOP/Exercise04/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Exercise04
+{
+    class Person
+    {
+        protected int age;
+        public void Greet(string Message)
+        {
+            System.Console.WriteLine(Message);
+        }
+        public void SetAge(int Age)
+        {
+            age = Age ;
+            //System.Console.WriteLine("Age is: " + Age );
+        }
+    }
+    class Student : Person
+    {
+        public void study()
+        {
+            System.Console.WriteLine("I'm studying");
+        }
+        public void ShowAge()
+        {
+            System.Console.WriteLine("My age is: " + age + " years old");
+        }
+    }
+    class Professor : Person
+    {
+        public void Explain()
+        {
+            System.Console.WriteLine("I'm Explaining");
+        }
+        public void ShowAge()
+        {
+            System.Console.WriteLine("My age is: " + age + " years old");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Greet("Hello");
+
+            Student student = new Student();
+            student.Greet("Hello");
+
+            Professor teacher = new Professor();
+            teacher.SetAge(25);
+            teacher.ShowAge();
+            teacher.Greet("Hello");
+            teacher.Explain();
+        }
+    }
+}
